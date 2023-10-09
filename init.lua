2c62603c3ea23743abe887ffc0375318d2f5bad3
AndresThePerez <andres@andrestheperez.com>
2023-10-09 08:08:15 -0500

    custom header/copy paste functionality

--- init.lua
+++ init.lua
@@ -57,6 +57,15 @@ return {
 
   plugins = {
     { "mfussenegger/nvim-dap", enabled = true },
+    {
+      "goolord/alpha-nvim",
+      opts = function(_, opts) -- override the options using lazy.nvim
+        opts.section.header.val = { -- change the header section value
+          "    My Custom ",
+          " Dashboard Header",
+        }
+      end,
+    },
   },
   -- Configure require("lazy").setup() options
   lazy = {
